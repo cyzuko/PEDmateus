8f67e8cffdfeda1b90a2422470e40f354014a30f
cyzuko <mateus23viana@gmail.com>
2025-05-09 19:55:06 +0100

    gg

--- nome-do-projeto/resources/views/home.blade.php
+++ nome-do-projeto/resources/views/home.blade.php
@@ -7,7 +7,7 @@
 @stop
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <h2>Bem-vindo, {{ Auth::user()->name }}!</h2>
 
     <div class="my-4">
@@ -15,7 +15,7 @@
         
         @if($faturas->count() > 0)
             <div class="table-responsive">
-                <table class="table table-striped">
+                <table class="table table-bordered table-striped">
                     <thead>
                         <tr>
                             <th>Fornecedor</th>
@@ -35,11 +35,149 @@
                 </table>
             </div>
             
-            <a href="{{ route('faturas.index') }}" class="btn btn-primary">Ver todas as faturas</a>
+            <div class="d-flex justify-content-center">
+                <a href="{{ route('faturas.index') }}" class="btn btn-primary">Ver todas as faturas</a>
+            </div>
         @else
             <p>Você ainda não tem faturas registradas.</p>
-            <a href="{{ route('faturas.create') }}" class="btn btn-success">Adicionar sua primeira fatura</a>
+            <div class="d-flex justify-content-center">
+                <a href="{{ route('faturas.create') }}" class="btn btn-success">Adicionar sua primeira fatura</a>
+            </div>
         @endif
     </div>
 </div>
+@stop
+
+@section('body')
+<div class="wrapper">
+
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name', 'Sistema de Faturas') }}
+            </a>
+            <button class="navbar-toggler order-1" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav me-auto">
+                    <!-- You can add additional left navbar items here if needed -->
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">Registro</a>
+                        </li>
+                    @else
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('home') }}">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('faturas.index') }}">Faturas</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                {{ Auth::user()->name }}
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}">
+                                    Logout
+                                </a>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+        <a href="{{ url('/') }}" class="brand-link">
+            <span class="brand-text font-weight-light">{{ config('app.name', 'Sistema de Faturas') }}</span>
+        </a>
+
+        <!-- Sidebar -->
+        <div class="sidebar">
+            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                <div class="info">
+                    <a href="#" class="d-block">{{ Auth::user()->name }}</a>
+                </div>
+            </div>
+
+            <!-- Sidebar Menu -->
+            <nav class="mt-2">
+                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                    <li class="nav-item">
+                        <a href="{{ route('home') }}" class="nav-link">
+                            <i class="nav-icon fas fa-home"></i>
+                            <p>Home</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route('faturas.index') }}" class="nav-link">
+                            <i class="nav-icon fas fa-file-invoice"></i>
+                            <p>Faturas</p>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </aside>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">{{ $title ?? 'Dashboard' }}</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="{{ url('/') }}">Home</a></li>
+                            <li class="breadcrumb-item active">{{ $title ?? 'Dashboard' }}</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                @if (session('success'))
+                    <div class="alert alert-success">
+                        {{ session('success') }}
+                    </div>
+                @endif
+                @yield('content')
+            </div><!-- /.container-fluid -->
+        </section>
+    </div><!-- /.content-wrapper -->
+
+    <!-- Footer -->
+    <footer class="main-footer">
+        <div class="container-fluid">
+            <div class="float-right d-none d-sm-block">
+                <b>Version</b> 3.2.0
+            </div>
+            <strong>&copy; {{ date('Y') }} <a href="#">Sistema de Faturas</a>.</strong> Todos os direitos reservados.
+        </div><!-- /.container-fluid -->
+    </footer>
+</div><!-- ./wrapper -->
+
+<!-- Scripts -->
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2.0/dist/js/adminlte.min.js"></script>
 @stop
