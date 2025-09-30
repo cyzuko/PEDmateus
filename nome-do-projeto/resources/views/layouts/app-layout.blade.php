b3879424beeb5fc54cef3748102f11feba817e47
cyzuko <mateus23viana@gmail.com>
2025-09-30 21:29:41 +0100

    rrr

--- nome-do-projeto/resources/views/layouts/app-layout.blade.php
+++ nome-do-projeto/resources/views/layouts/app-layout.blade.php
@@ -20,7 +20,7 @@
         </main>
 
         <footer class="bg-blue-600 text-white p-4 text-center">
-            © {{ date('Y') }} Eureka
+            © {{ date('Y') }} eureka
         </footer>
     </div>
     
