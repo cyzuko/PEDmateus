898ffdfd369458bb2b5e2e45f5881674f622f914
cyzuko <mateus23viana@gmail.com>
2025-10-01 18:18:33 +0100

    b

--- nome-do-projeto/routes/web.php
+++ nome-do-projeto/routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\FaturaController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ExplicacaoController;
 
